79785f30e34f85d93a4a6204a50053f8df8f58e2
AndrejBratic <andrej.bratic@prvabeogim.edu.rs>
2022-05-05 00:01:22 +0200

    mozda i za maturski?

--- Kreiranje baze.sql
+++ Kreiranje baze.sql
@@ -1,8 +1,13 @@
+create database fudbalske_slicice;
+
+
 use fudbalske_slicice
+
+
 create table Korisnik
 (id int primary key identity(1,1), username varchar(30) not null, ime nvarchar(30), prezime nvarchar(30), email varchar(30), pass varchar(30))
 
-create table godina_izdanja
+create table Godina_izdanja
 (id int primary key identity(1,1), naziv varchar(4))
 
 create table Izdavac
@@ -12,7 +17,7 @@ create table Album
 (id int primary key identity(1,1), naziv varchar(30), izdavac_id int foreign key references Izdavac(id), godina_izdanja_id int foreign key references godina_izdanja (id))
 
 create table Slicica
-(id int primary key identity(1,1), ime varchar(30), prezime varchar(30), album_id int foreign key references Album (id))
+(id int primary key identity(1,1), ime varchar(30), prezime varchar(30), broj int, slika varbinary(max),  album_id int foreign key references Album (id))
 
 create table Slicica_Korisnik
 (id int primary key identity(1,1), korisnik_id int foreign key references Korisnik (id), slicica_id int foreign key references Slicica (id))
